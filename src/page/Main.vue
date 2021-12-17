6d8652640af3c672e00f3bba62ff27571982e0d4
zhangqiyu <paulzhangqiyu@163.com>
2021-12-17 17:56:15 +0800

    提交】

--- src/page/Main.vue
+++ src/page/Main.vue
@@ -43,12 +43,87 @@ export default {
   },
   mounted () {
     this.getData()
+    this.text()
   },
   methods: {
     getData () {
       getlist({ pageNo: 1 }).then(res => {
-        console.log(22, res)
       })
+    },
+
+    text () {
+      let arr = [
+        { id: 2, name: '部门2', pid: 1 },
+        { id: 3, name: '部门3', pid: 1 },
+        { id: 5, name: '部门5', pid: 4 },
+        { id: 4, name: '部门4', pid: 3 },
+        { id: 1, name: '部门1', pid: 0 },
+      ]
+
+      let obj = {
+        name: '111',
+        age: '222',
+        height: '180'
+      }
+      //对对象的取值  用结构
+      const { name: mingz } = obj || {}
+
+      //对象 合并    用 Object.assign(目标对象,。。)
+      const a = {
+        name: 1,
+        arr: {
+          gg: 'gg'
+        }
+      }
+      const b = {
+        age: 22
+      }
+      const c = Object.assign(a, b)
+      // 循环对象---》 返回所有的属性值的数组  
+      // Object.values(obj)
+      // 获取 嵌套对象的属性值
+      //  n_val = obj?.attr1?.attr2
+      //  const val = a ?.arr ?.gg
+
+
+
+
+
+
+      // 数组合并    用拓展运算符 
+      const aaa = [{ id: 2, name: '部门233', pid: 1 }]
+      // const bbb = [...arr, ...aaa]
+
+      // 去重   ... set
+      let bbb = [...new Set(bbb), ...new Set(arr)]
+      //数组中是否包含某个元素  
+      const ccc = [2, 34, 3, 5]
+      ccc.includes(2)   //   ccc.indexOf(2) find() filter()
+      //数组筛选
+      // filtre((v) =>{ return v.a === b})
+      //    find()
+
+      // 数组扁平化 多维变一维  flat(1)  
+
+
+
+      // 字符串拼接  模板字符串  `${}` 
+      const cc = '小明'
+      const dd = 66
+      const rs = `${cc}的成绩${dd >= 60 ? '合格' : '不合格'}`
+
+
+      const deps = {  //扁平化数组   flat(1)
+        '采购部': [1, 2, 3],
+        '人事部': [5, 8, 12],
+        '行政部': [5, 14, 79],
+        '运输部': [3, 64, 105],
+      }
+      // console.log(999, ...Object.values(deps).flat(1))
+
+
+
+
     },
   },
 }
